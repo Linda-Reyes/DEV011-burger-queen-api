244a10425e523209bd07c5668be80ac256ebdc75
Linda Reyes <lindamreyesr@gmail.com>
2024-01-09 20:59:50 -0500

    instalar mongo y compass

--- connect.js
+++ connect.js
@@ -1,9 +1,19 @@
 const config = require('./config');
-
+const { MongoClient } = require('mongodb');
 // eslint-disable-next-line no-unused-vars
+
 const { dbUrl } = config;
+const client = new MongoClient(dbUrl);
 
 async function connect() {
+  try {
+    await client.connect();
+    const db = client.db('Burger-Queen');
+    return db;
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+    throw new Error('No se pudo conectar a la base de datos');
+  }
   // TODO: Database Connection
 }
 
