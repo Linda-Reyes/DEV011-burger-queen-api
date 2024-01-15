745ad6a31216fa40fadfb6dda88c645fa25c8107
Linda Reyes <lindamreyesr@gmail.com>
2024-01-15 06:47:38 -0500

    conectar con la base de datos

--- connect.js
+++ connect.js
@@ -3,18 +3,18 @@ const { MongoClient } = require('mongodb');
 // eslint-disable-next-line no-unused-vars
 
 const { dbUrl } = config;
+// extrae la variable de dbUrl de config.
 const client = new MongoClient(dbUrl);
 
 async function connect() {
   try {
     await client.connect();
     const db = client.db('Burger-Queen');
+    console.log('La conexión a la base de datos fue exitosa');
     return db;
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
-    throw new Error('No se pudo conectar a la base de datos');
+    console.error('Error durante la conexión:', error);
+    throw new Error('Lo siento, no se pudo conectar a la base de datos');
   }
-  // TODO: Database Connection
 }
-
 module.exports = { connect };
