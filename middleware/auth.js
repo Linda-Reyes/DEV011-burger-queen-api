461a8fe0f1014de4e19be33e76058b99a25981f7
Linda Reyes <lindamreyesr@gmail.com>
2024-01-25 00:53:49 -0500

    Implementación middleware de autenticación fixes #5

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 
+// Middleware principal
 module.exports = (secret) => (req, resp, next) => {
   const { authorization } = req.headers;
-
   if (!authorization) {
     return next();
   }
@@ -15,29 +15,51 @@ module.exports = (secret) => (req, resp, next) => {
 
   jwt.verify(token, secret, (err, decodedToken) => {
     if (err) {
-      return next(403);
+      console.log(token, err);
+      return next({ status: 403, message: 'Token invalido' });
     }
-
-    // TODO: Verify user identity using `decodeToken.uid`
+    req.userId = decodedToken.uid;
+    req.userRole = decodedToken.role;
+    console.log('Usuario autenticado:', req.userId, 'con el rol de:', req.userRole);
+    return next();
   });
+
+  // TODO: Verify user identity using `decodeToken.uid`//
 };
 
-module.exports.isAuthenticated = (req) => (
-  // TODO: Decide based on the request information whether the user is authenticated
-  false
-);
+// Función para verificar si el usuario está autenticado
+module.exports.isAuthenticated = (req) => {
+  const userId = req.userId ? req.userId.toString() : null;
+  if (userId) {
+    console.log('Usuario autenticado:', userId);
+    return true;
+  }
+  console.log('Usuario no autenticado');
+  return false;
+  // TODO: Decide based on the request information whether the user is authenticated //
+};
 
-module.exports.isAdmin = (req) => (
+// Función para verificar si el usuario es administrador
+module.exports.isAdmin = (req) => {
+  const userRole = req.userRole ? req.userRole : null;
+  if (userRole === 'admin') {
+    console.log('El usuario es administrador');
+    return true;
+  }
+  console.log('El usuario no es administrador');
+  return false;
   // TODO: Decide based on the request information whether the user is an admin
-  false
-);
+};
 
+// Middleware para requerir autenticación
 module.exports.requireAuth = (req, resp, next) => (
   (!module.exports.isAuthenticated(req))
+
     ? next(401)
     : next()
 );
 
+// Middleware para requerir que el usuario sea administrador
 module.exports.requireAdmin = (req, resp, next) => (
   // eslint-disable-next-line no-nested-ternary
   (!module.exports.isAuthenticated(req))
