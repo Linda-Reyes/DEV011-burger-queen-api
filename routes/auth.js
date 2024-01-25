7fa6d6caa9d9ede33c5c576f4993878e944f9f32
Linda Reyes <lindamreyesr@gmail.com>
2024-01-25 00:50:00 -0500

    Autentificación del usuario fixes #4

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,31 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
-
 const { secret } = config;
+const { connect } = require('../connect');
 
 module.exports = (app, nextMain) => {
-
-  app.post('/login', (req, resp, next) => {
+  app.post('/login', async (req, resp, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
       return next(400);
     }
 
-    // TODO: Authenticate the user
-    // It is necessary to confirm if the email and password
-    // match a user in the database
-    // If they match, send an access token created with JWT
-
-    next();
+    try {
+      const db = connect();
+      const users = db.collection('users');
+      const query = { email, password };
+      const resultUser = await users.findOne(query);
+      console.log(resultUser);
+      if (resultUser) {
+        const token = jwt.sign({ email, uid: resultUser._id, role: resultUser.role }, secret);
+        resp.json({ accessToken: token });
+      } else {
+        next(404);
+      }
+    } catch (err) {
+      console.error(err);
+    }
   });
 
   return nextMain();
